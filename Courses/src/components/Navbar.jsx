46598a0682ea24c9e5a67ba246fe3770090edb9c
Shaik Moin <moinshaik1603@gmail.com>
2025-01-02 22:06:46 +0530

    Courses

---
+++ Courses/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="bg-red-400 py-4">
+        <h1 className="text-center text-3xl font-bold text-white">
+          Top Courses
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
