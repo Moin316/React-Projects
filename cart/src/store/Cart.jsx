ffa56e783490c430e91132e43a926e786d953284
Shaik Moin <moinshaik1603@gmail.com>
2025-01-16 22:31:40 +0530

    Redux Cart

---
+++ cart/src/store/Cart.jsx
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
+    items:[],
+    statusTab: false,
+}
+const CartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (item) => item.productId === productId
+      );
+      if (indexProductId >= 0) {
+        state.items[indexProductId].quantity += quantity;
+      } else {
+        state.items.push({ productId, quantity });
+      }
+      localStorage.setItem("carts", JSON.stringify(state.items));
+    },
+    changeQuantity(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (item) => item.productId === productId
+      );
+      if(quantity > 0) {
+        state.items[indexProductId].quantity = quantity; 
+      }
+      else{
+        // delete state.items[indexProductId];
+        state.items.filter(item => item.productId !== productId)
+      }
+    },
+    toggleStatusTab(state) {
+      if(state.statusTab === false){
+        state.statusTab = true;
+      }
+      else{
+        state.statusTab = false;
+      }
+    },
+  },
+});
+export const { addToCart, changeQuantity, toggleStatusTab } = CartSlice.actions;
+export default CartSlice.reducer;
---
+++ cart/src/store/index.jsx
@@ -0,0 +1,7 @@
+import { configureStore} from "@reduxjs/toolkit";
+import CartReducer from './Cart'
+export const store =configureStore({
+    reducer:{cart:CartReducer,}
+
+})
+export default store
